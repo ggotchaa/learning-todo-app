b8e4dfe992644c0c97b7f0c32772caba91c0ca7e
Nursultan <nurbekenov.nb@gmail.com>
2025-09-26 15:13:24 +0500

    act

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,31 +1,10 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, Inject, OnInit } from '@angular/core';
-
-interface NavItem {
-  label: string;
-  path: string;
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent implements OnInit {
-  isDarkTheme = false;
+export class HeaderComponent {
 
-  constructor(@Inject(DOCUMENT) private document: Document) {}
-
-  ngOnInit(): void {
-    this.isDarkTheme = this.document.documentElement.getAttribute('data-theme') === 'dark';
-  }
-
-  toggleTheme(): void {
-    this.isDarkTheme = !this.isDarkTheme;
-    if (this.isDarkTheme) {
-      this.document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-      this.document.documentElement.removeAttribute('data-theme');
-    }
-  }
 }
